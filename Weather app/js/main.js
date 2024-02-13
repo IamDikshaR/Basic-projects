cb3ce22c564df99b9ae89b5e621c95cacb48d97c
diksha <rajdiksha91@gmail.com>
2024-02-13 10:55:06 +0530

    home button

--- Weather app/js/main.js
+++ Weather app/js/main.js
@@ -2,7 +2,7 @@ import {
     addSpinner, displayError
 } from "./domFunctions.js";
 import {
-    setLocationObject
+    setLocationObject, getHomeLocation
 } from "./dataFunctions.js"
 import CurrentLocation from "./CurrentLocation.js";
 const currentLoc = new CurrentLocation();
@@ -12,8 +12,11 @@ const initApp = () => {
     const geoButton = document.getElementById("getLocation");
     geoButton.addEventListener("click", getGeoWeather);
 
+    const homeButton = document.getElementById("home");
+    homeButton.addEventListener("click", loadWeather);
     //set up
     //load weather
+    loadWeather()
 }
 
 document.addEventListener("DOMContentLoaded", initApp)
@@ -27,12 +30,12 @@ const getGeoWeather = (event) => {
     navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
   };
   
-  const geoError = (errObj) => {
+const geoError = (errObj) => {
     const errMsg = errObj ? errObj.message : "Geolocation not supported";
     displayError(errMsg, errMsg);
-  };
+};
   
-  const geoSuccess = (position) => {
+const geoSuccess = (position) => {
     const myCoordsObj = {
       lat: position.coords.latitude,
       lon: position.coords.longitude,
@@ -40,4 +43,41 @@ const getGeoWeather = (event) => {
     };
     setLocationObject(currentLoc, myCoordsObj);
     updateDataAndDisplay(currentLoc);
-  };
+};
+
+const loadWeather = (event) =>{
+    const savedLocation = getHomeLocation();
+    if(!savedLocation && !event) return getGeoWeather();
+    if(!savedLocation && event.type === "click"){
+        displayError(
+            "No Home Location Saved.",
+            "Sorry. Please save your home location first."
+          );
+        } else if (savedLocation && !event) {
+          displayHomeLocationWeather(savedLocation);
+        } else {
+          const homeIcon = document.querySelector(".home");
+          addSpinner(homeIcon);
+          displayHomeLocationWeather(savedLocation);
+        }
+};
+
+const displayHomeLocationWeather = (home) => {
+    if (typeof home === "string") {
+        const locationJson = JSON.parse(home);
+        const myCoordsObj = {
+            lat: locationJson.lat,
+            lon: locationJson.lon,
+            name: locationJson.name,
+            unit: locationJson.unit
+        };
+        setLocationObject(currentLoc, myCoordsObj);
+        updateDataAndDisplay(currentLoc);
+    }
+};
+
+const updateDataAndDisplay = async (locationObj) => {
+    // console.log(locationObj);
+    // const weatherJson = await getWeatherFromCoords(locationObj);
+    // if (weatherJson) updateDisplay(weatherJson, locationObj);
+};
