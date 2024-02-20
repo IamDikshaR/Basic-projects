ae4259af693d260e01db391241e430b45d1bb872
diksha <rajdiksha91@gmail.com>
2024-02-20 20:47:00 +0530

    added color

--- Counter/app.js
+++ Counter/app.js
@@ -6,15 +6,35 @@ const value= document.getElementById("value");
 
 reset.addEventListener("click", ()=>{
     value.textContent="0";
+    color()
 })
 dec.addEventListener("click", ()=>{
     value.textContent= value.textContent-1;
+    color()
 })
 
 inc.addEventListener("click", ()=>{
     let number = parseInt(value.textContent);
     // number+=1;
     value.textContent= number+1;
+    color();
 })
+
+
+function color() {
+    let number = parseInt(value.textContent);
+    if(number>0){
+        value.style.color= "green"
+    }
+    
+    if(number<0){
+        value.style.color= "red"
+    }
+    
+    if(number===0){
+        value.style.color= "#222"
+    }
+}
+
 
 
