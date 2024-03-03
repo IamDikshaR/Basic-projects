f3b3372f6978c62e110b002aa68dd6a2f94816b6
diksha <rajdiksha91@gmail.com>
2024-03-03 23:54:51 +0530

    dropdown selection

--- Ice cream store/app.js
+++ Ice cream store/app.js
@@ -1,16 +1,24 @@
-let stocks={
-    fruits: ["strawberry", "mango", "custard", "orange", "pineapple", "kiwi", "coconut"],
-    liquid: ["water", "milk","ice"],
-    holder: ["cone", "cup", "waffle"],
-    toppings: ["chocochip", "sprinkles", "jelly", "fruit", "peanuts", "nuts"]
-}
+let stocks = {
+  fruits: [
+    "strawberry",
+    "mango",
+    "custard",
+    "orange",
+    "pineapple",
+    "kiwi",
+    "coconut",
+  ],
+  liquid: ["water", "milk", "ice"],
+  holder: ["cone", "cup", "waffle"],
+  toppings: ["chocochip", "sprinkles", "jelly", "fruit", "peanuts", "nuts"],
+};
 
 let is_shop_open = true;
 
-const open= document.querySelector(".open");
-const close= document.querySelector(".close");
-const display= document.querySelector(".main");
-const load= document.querySelector(".load");
+const open = document.querySelector(".open");
+const close = document.querySelector(".close");
+const display = document.querySelector(".main");
+const load = document.querySelector(".load");
 const btn = document.querySelector(".submit");
 const selection = document.querySelector(".selection");
 
@@ -21,86 +29,107 @@ var holder = document.getElementById("myHolder");
 var selectedOption = holder.options[holder.selectedIndex];
 var selectedHolder = selectedOption.value;
 
+function saveSelectedValue([...args]) {
+  for (let i = 0; i < args.length; i++) {
+    var dropdown = document.getElementById("args[i]");
+    // Get the selected option
+    var selectedOption = dropdown.options[dropdown.selectedIndex];
+    // Get the value of the selected option
+    var selectedValue = selectedOption.value;
 
-function getSelectedValues() {
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-    var selectedValues = [];
-    checkboxes.forEach(function(checkbox) {
-      selectedValues.push(checkbox.value);
-    });
-    // console.log("Selected Values: " + selectedValues.join(', '));
-    return selectedValues.join(', ');
+    return selectedValue;
   }
-
-let order = (time, work) =>{
-    return new Promise((resolve, reject)=>{
-        if(is_shop_open){
-            display.classList.add("hide");
-            setTimeout(()=>{
-                resolve(work())
-            }, time);
-        }
-        else{
-            reject(new Error("Shop is closed"));
-        }
-    })
 }
 
-(function (){
-    if(!is_shop_open){
-        open.classList.add("hide");
-        close.classList.remove("hide");
-        selection.classList.add("hide");
+function getSelectedValues() {
+  var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+  var selectedValues = [];
+  checkboxes.forEach(function (checkbox) {
+    selectedValues.push(checkbox.value);
+  });
+  // console.log("Selected Values: " + selectedValues.join(', '));
+  return selectedValues.join(", ");
+}
+
+let order = (time, work) => {
+  return new Promise((resolve, reject) => {
+    if (is_shop_open) {
+      display.classList.add("hide");
+      setTimeout(() => {
+        resolve(work());
+      }, time);
+    } else {
+      reject(new Error("Shop is closed"));
     }
+  });
+};
+
+(function () {
+  if (!is_shop_open) {
+    open.classList.add("hide");
+    close.classList.remove("hide");
+    selection.classList.add("hide");
+  }
 })();
 
-btn.addEventListener("click", ()=>{
-order(2000, ()=>console.log(`Order is being placed and ${selectedFruit} has been Selected`))
+btn.addEventListener("click", () => {
+  order(2000, () =>
+    console.log(
+      `Order is being placed and ${saveSelectedValue(
+        "myFruit"
+      )} has been Selected`
+    )
+  )
+    .then(() => {
+      load.textContent = "Production has started";
+      return order(1000, () => console.log("Production has started"));
+    })
 
-.then(()=>{
-    load.textContent = "Production has started";
-    return order(1000, ()=>console.log("Production has started"))
-})
+    .then(() => {
+      load.textContent = `${saveSelectedValue("myFruit")} has been chopped`;
+      return order(2000, () => console.log("The fruit has been chopped"));
+    })
 
-.then(()=>{
-    load.textContent = `${selectedFruit} has been chopped`;
-    return order(2000, ()=>console.log("The fruit has been chopped"))
-})
+    .then(() => {
+      load.textContent = "Water, ice and milk were added";
+      return order(1000, () => console.log("Water,ice and milk were added"));
+    })
 
-.then(()=>{
-    load.textContent = "Water, ice and milk were added";
-    return order(1000, ()=>console.log("Water,ice and milk were added"))
-})
+    .then(() => {
+      load.textContent = "Starting the machine";
+      return order(1000, () => console.log("Starting the machine"));
+    })
 
-.then(()=>{
-    load.textContent = "Starting the machine";
-    return order(1000, ()=>console.log("Starting the machine"))
-})
+    .then(() => {
+      load.textContent = `Ice-cream placed on ${saveSelectedValue("myHolder")}`;
+      return order(2000, () =>
+        console.log(`Ice-cream placed on ${stocks.holder[0]}`)
+      );
+    })
 
-.then(()=>{
-    load.textContent = `Ice-cream placed on ${selectedHolder}`;
-    return order(2000, ()=>console.log(`Ice-cream placed on ${stocks.holder[0]}`))
-})
+    .then(() => {
+      getSelectedValues();
+      load.textContent = `${getSelectedValues()} was selected`;
+      return order(3000, () =>
+        console.log(`${stocks.toppings[0]} was selected`)
+      );
+    })
 
-.then(()=>{
-    getSelectedValues();
-    load.textContent = `${getSelectedValues()} was selected`;
-    return order(3000, ()=>console.log(`${stocks.toppings[0]} was selected`))
-})
+    .then(() => {
+      load.textContent = "Ice cream is ready and served!";
+      return order(1000, () => console.log("Ice cream is ready and served"));
+    })
 
-.then(()=>{
-    load.textContent = "Ice cream is ready and served!";
-    return order(1000, ()=>console.log("Ice cream is ready and served"))
-})
+    .catch(() => {
+      // load.textContent = "Customer left";
+      console.log("customer left");
+    })
 
-.catch(()=>{
-    // load.textContent = "Customer left";
-    console.log("customer left");
-})
-
-.finally(()=>{
-    load.textContent = "Thank you for visiting 🤗!!!";
-    console.log("Done");
-})
-})
-
+    .finally(() => {
+      load.textContent = "Thank you for visiting 🤗!!!";
+      // setTimeout(()=> window.addEventListener("DOMContentLoaded", ()=>{
+      //         display.classList.remove("hide");
+      // }), 2000)
+      console.log("Done");
+    });
+});
